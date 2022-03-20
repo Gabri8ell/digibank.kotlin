06c5d8593f303cbcb1d3f1eb3fea0447db13def5
Gabri8ell <gbr192008@hotmail.com>
2022-03-20 15:27:14 -0300

    Add in class Pessoa.kt private set, constructor() and function pessoaDetail what list detail of Pessoa ob

--- src/main/kotlin/one/digibank/Pessoa.kt
+++ src/main/kotlin/one/digibank/Pessoa.kt
@@ -3,12 +3,17 @@ package one.digibank
 class Pessoa {
     var name: String = "Jhenifer"
     var cpf: String = "123.456.789-25"
+    private set
+
+    constructor()
+
+    fun pessoaDetail():String = "Nome : $name \nCPF: $cpf"
 
 }
 
 fun main(){
     val milacam = Pessoa()
 
-    println(milacam.name)
-    println(milacam.cpf)
+    println(milacam.pessoaDetail())
+
 }
