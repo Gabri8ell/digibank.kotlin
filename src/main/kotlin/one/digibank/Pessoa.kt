0e7edd883afc75a81a1a966d7418fa85f1dc094d
Gabri8ell <gbr192008@hotmail.com>
2022-03-20 14:53:54 -0300

    Creation Class Pessoa

---
+++ src/main/kotlin/one/digibank/Pessoa.kt
@@ -0,0 +1,14 @@
+package one.digibank
+
+class Pessoa {
+    var name: String = "Jhenifer"
+    var cpf: String = "123.456.789-25"
+
+}
+
+fun main(){
+    val milacam = Pessoa()
+
+    println(milacam.name)
+    println(milacam.cpf)
+}
